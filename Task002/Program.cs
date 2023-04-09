cbdf210868c76a314a6e0d0501f4998d631ea203
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-04-09 21:13:24 +0300

    решение задачи №36

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -5,5 +5,42 @@
 
 [-4, -6, 89, 6] -> 0 */
 
+ Console.Clear();
 
+Console.WriteLine("Введите размер массива: ");
+int length = ReadNumberFromConsole();
+int[] numbers = new int[length];
+FillArrayRandomNumbers(numbers);
+Console.Write("Мы получили массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int n = 1; n < numbers.Length; n+=2)
+    sum = sum + numbers[n];
+
+    Console.WriteLine($"всего в нём {numbers.Length} чисел, из которых сумма чисел на позициях с нечётным индексом = {sum}");
+
+int ReadNumberFromConsole()
+{
+  string input = Console.ReadLine();
+  return int.Parse(input);
+}
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"{numbers[i]} ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
 
